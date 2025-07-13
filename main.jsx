2f11313eab0f7807d9c6c8378c7f5a3c4c3dbb47
jaskayrat <jaskayrat@icloud.com>
2025-07-13 12:09:13 +0500

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// Этот файл является точкой входа в ваше приложение.
-// Он находит элемент с id 'root' в index.html и рендерит в нем главный компонент App.
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
