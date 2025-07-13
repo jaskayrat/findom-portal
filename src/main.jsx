18eb35b76222d831ad3ff848491c5855daad8786
jaskayrat <jaskayrat@icloud.com>
2025-07-13 12:12:08 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from &#39;react&#39;
-import ReactDOM from &#39;react-dom/client&#39;
-import App from &#39;./App.jsx&#39;
-import &#39;./index.css&#39;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-\<React.StrictMode\>
-&lt;App /&gt;
-\</React.StrictMode\>,
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
